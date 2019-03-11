a43797956167da83234d77911cec168d9492da70
lxdnz254 <lxdnz34@gmail.com>
2019-03-11 22:09:05 +1300

    Completed Profiles for User

--- conduit/urls.py
+++ conduit/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('conduit.apps.authentication.urls', namespace='authentication')),
+    path('api/', include('conduit.apps.profiles.urls', namespace='profiles'))
 ]
